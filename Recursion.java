2eb9a46e9cc3a2acd8c427422cb070ada4fe3f14
Usernameisyangqilin <45375840+Usernameisyangqilin@users.noreply.github.com>
2018-12-27 14:55:08 +0800

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -15,3 +15,31 @@ public class Recursion{
 
 
 }
+public class recursion{
+
+    public static void rec(int n){
+    
+        System.out.println(n);
+        if(n == 1){
+    
+    return;
+    
+        }
+        rec(n-1);
+        System.ou.println(n);
+    }
+    
+    public static int factorial(int n){
+    if(n ==1 || n == 0){
+        return 1;
+    }
+    return n * factorial(n - 1);
+    }
+    
+    public static void main(String[] args){
+    
+        int n = 3;                                                     
+        rec(n); 
+    
+    }
+    }
